21a9fdb3c38eef98753f1c3c6f38c995bee0f76c
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-18 18:57:32 -0500

    created trips controller and request spec

---
+++ app/controllers/api/v1/trips_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::TripsController < ApplicationController
+  def create
+    if params[:api_key].nil? || User.find_by(api_key: params[:api_key]).nil?
+      render status: 401, json: { errors: "Unauthorized"}
+    else
+      road_trip = RoadTripFacade.fetch_road_trip_info(route_params)
+      render status: 201, json: RoadtripSerializer.new(road_trip)
+    end
+  end
+
+  private
+
+  def route_params
+    params.permit(:origin, :destination)
+  end
+end
