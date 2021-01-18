213a54da4578e6f835f3e8aa68ac598e39303e68
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-18 18:56:39 -0500

    Created road trip facade and spec

---
+++ app/facades/road_trip_facade.rb
@@ -0,0 +1,16 @@
+class RoadTripFacade
+  class << self
+    def fetch_road_trip_info(trip_info)
+      road_trip_route = MapQuestService.fetch_route(trip_info)
+      destination_coords = nil
+      destination_weather = nil
+
+      if road_trip_route[:info][:statuscode] != 402
+        destination_coords = MapQuestService.fetch_coordiantes(trip_info[:destination])
+        destination_weather = OpenWeatherService.fetch_location_weather(destination_coords)
+      end
+      
+      RoadTrip.new(trip_info, road_trip_route[:route][:realTime], destination_weather)
+    end
+  end
+end
