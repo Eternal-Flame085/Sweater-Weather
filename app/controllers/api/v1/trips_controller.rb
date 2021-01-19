3f55beda4bd011ad65bae555fa9f30eac5578fe8
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-18 21:00:48 -0500

    Fixed small error

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::TripsController < ApplicationController
   def create
+    units = 'imperial'
     units = params[:units] if !params[:units].nil?
     if params[:api_key].nil? || User.find_by(api_key: params[:api_key]).nil?
       render status: 401, json: { errors: "Unauthorized"}
     else
-      road_trip = RoadTripFacade.fetch_road_trip_info(route_params, units = 'imperial')
+      road_trip = RoadTripFacade.fetch_road_trip_info(route_params, units)
       render status: 201, json: RoadtripSerializer.new(road_trip)
     end
   end
