edd9f7419aee43cd924e1b8ddf51ff5e5678a47e
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-19 11:22:47 -0500

    Created request spec for munchies

---
+++ spec/requests/api/v1/munchies_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe "trips controller" do
+  VCR.use_cassette('pueblo_co_coords') do
+    VCR.use_cassette('pueblo_weather_data') do
+      it "returns a serialized road trip poro if there is a possible route and valid api key is given" do
+
+        get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"
+
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+        munchies = JSON.parse(response.body, symbolize_names: true)
+
+        expect(munchies).to be_a(Hash)
+        expect(munchies[:data]).to be_a(Hash)
+        expect(munchies[:data].keys).to eq([:id, :type, :attributes, :restaurant])
+        expect(munchies[:data][:type]).to eq("munchies")
+        expect(munchies[:data][:attributes]).to be_a(Hash)
+
+        expect(munchies[:data][:attributes][:destination_city]).to be_a(String)
+        expect(munchies[:data][:attributes][:travel_time]).to be_a(String)
+        expect(munchies[:data][:attributes][:forecast]).to be_a(Hash)
+        expect(munchies[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+        expect(munchies[:data][:attributes][:forecast][:conditions]).to be_a(String)
+        expect(munchies[:data][:attributes][:forecast][:temperature]).to be_a(String)
+
+        expect(munchies[:data][:restaurant].keys).to eq([:name, :address])
+        expect(munchies[:data][:restaurant][:name]).to be_a(String)
+        expect(munchies[:data][:restaurant][:address]).to be_a(String)
+      end
+    end
+  end
+end
