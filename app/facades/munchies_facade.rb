4d6d34eaaf396a7e8e8101409aae813c8dd53fbb
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-19 13:02:47 -0500

    Renamed a few variables

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -3,9 +3,10 @@ class MunchiesFacade
     def find_food_for_destination(munchies_route_info)
       route =  {origin: munchies_route_info['start'], destination: munchies_route_info['end']}
       munchies_trip_route = MapQuestService.fetch_route(route)
-      destination_coords = MapQuestService.fetch_coordiantes(trip_info[:destination])
+      destination_coords = MapQuestService.fetch_coordiantes(route[:destination])
       destination_weather = OpenWeatherService.fetch_location_weather(destination_coords)
-      restaurant = YelpService.find_restaurant(munchies_route_info['end'], munchies_route_info['food'])
+      time_of_arrival = destination_weather[:current][:dt] + munchies_trip_route[:route][:realTime]
+      restaurant = YelpService.find_restaurant(route[:destination], munchies_route_info['food'], time_of_arrival)
       require "pry"; binding.pry
     end
   end
