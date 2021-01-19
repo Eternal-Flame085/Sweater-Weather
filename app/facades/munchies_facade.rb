c25113863719fc6f0ac6edd9e25c87bd72486858
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-19 11:59:47 -0500

    Working on making the facade spec and facade

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,7 +1,10 @@
 class MunchiesFacade
   class << self
     def find_food_for_destination(munchies_route_info)
-      munchies_trip_route = MapQuestService.fetch_route()
+      require "pry"; binding.pry
+      route =  {origin: munchies_route_info[:start], destination: munchies_route_info[:end]}
+      munchies_trip_route = MapQuestService.fetch_route(route)
+      require "pry"; binding.pry
     end
   end
 end
---
+++ spec/facades/munchies_facade_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe MunchiesFacade do
+  it "returns a poro object" do
+    VCR.use_cassette('map_quest_api_denver_pueblo') do
+      munchies_route_info = {"start"=>"denver,co", "end"=>"pueblo,co", "food"=>"chinese"}
+      munchies = MunchiesFacade.find_food_for_destination(munchies_route_info)
+      require "pry"; binding.pry
+      expect(munchies).to be_a(Hash)
+      expect(munchies[:data]).to be_a(Hash)
+      expect(munchies[:data].keys).to eq([:id, :type, :attributes, :restaurant])
+      expect(munchies[:data][:type]).to eq("munchies")
+      expect(munchies[:data][:attributes]).to be_a(Hash)
+
+      expect(munchies[:data][:attributes][:destination_city]).to be_a(String)
+      expect(munchies[:data][:attributes][:travel_time]).to be_a(String)
+      expect(munchies[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(munchies[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+      expect(munchies[:data][:attributes][:forecast][:conditions]).to be_a(String)
+      expect(munchies[:data][:attributes][:forecast][:temperature]).to be_a(String)
+
+      expect(munchies[:data][:restaurant].keys).to eq([:name, :address])
+      expect(munchies[:data][:restaurant][:name]).to be_a(String)
+      expect(munchies[:data][:restaurant][:address]).to be_a(String)
+    end
+  end
+end
